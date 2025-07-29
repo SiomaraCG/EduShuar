21bbce58e23741007b314756db59c7d8466f9300
Jair1031 <jairalejandromercha@gmail.com>
2025-07-28 21:30:34 -0500

    Header

--- src/app/app.ts
+++ src/app/app.ts
@@ -6,13 +6,6 @@ import { Footer } from './core/layout/footer/footer';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, Header, Footer],
-=======
-import { Footer } from './core/layout/footer/footer';
-
-@Component({
-  selector: 'app-root',
-  imports: [RouterOutlet,Footer ],
->>>>>>> 7aad5c11b44b7d3cfeb27681d1fdfd758bdda343
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
