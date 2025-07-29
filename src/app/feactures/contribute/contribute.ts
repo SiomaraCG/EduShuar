777a7f6da18b1d7a16486a3e37c650d166fe19f0
SiomaraCG <desireesiomara@gmail.com>
2025-07-28 20:14:25 -0500

    creación de componentes

---
+++ src/app/feactures/contribute/contribute.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contribute',
+  imports: [],
+  templateUrl: './contribute.html',
+  styleUrl: './contribute.scss'
+})
+export class Contribute {
+
+}
